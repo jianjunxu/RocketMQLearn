b9b75346def8f31895811cc8dabdbbe578e04bce
xujianjun <xujj@jiuxiulvxing.com>
2016-08-19 16:04:16 +0800

    init

---
+++ src/main/java/com/jayden/jxlx/Main.java
@@ -0,0 +1,13 @@
+package com.jayden.jxlx;
+
+/**
+ * User : jianjun.xu
+ * Date : 2016/8/19
+ * Time : 15:51
+ * Desc :
+ */
+public class Main {
+	public static void main(String[] args){
+		System.out.println("hello world!");
+	}
+}
